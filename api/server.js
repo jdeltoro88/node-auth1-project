14f701790baddc80fff15ce4a3892c509282ba06
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-04-08 17:36:21 -0600

    server updates

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 
 const usersRouter = require("./users/users-router.js");
 const authRouter = require("./auth/auth-router.js");
@@ -23,9 +25,33 @@ const authRouter = require("./auth/auth-router.js");
 
 const server = express();
 
+const sessionConfig = {
+  name: 'chocolatechip',
+  secret: 'the secret',
+  cookie: {  
+    maxAge: 3600000,
+		secure: false,
+		httpOnly: true,
+	},
+	resave: false,
+	saveUninitialized: false,
+
+	store: new KnexSessionStore({
+		knex: require('../data/db-config'),
+		tablename: 'sessions',
+		sidfieldname: 'sid',
+		createtable: true,
+		clearInterval: 3600000,
+	}),
+};
+
+
+  
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.use("/api/users", usersRouter);
 server.use("api/auth", authRouter);
